ea0613baa2821478d973f374b58a05427884f764
Philipp Klenze <pklenze@ph.tum.de>
2023-02-20 16:38:29 +0100

    segfault

---
+++ segfault/segfault.cxx
@@ -0,0 +1,22 @@
+void A(int* p)
+{
+  // do something illegal:
+  (*p)++;
+}
+void B(int * a)
+{
+  A(a);
+}
+
+void C(bool crash=1)
+{
+  int* p=nullptr;
+  int i{};
+  if (!crash)
+    p=&i;
+  B(p);
+}
+int main()
+{
+  C(0);
+}
